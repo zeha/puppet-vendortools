ab80254a03da553d217b57e709aba9357e6112a8
Jeff Palmer <jeff@palmerit.net>
2014-04-24 21:51:48 +0000

    Cleanup to comply with puppet lint.

--- manifests/dell.pp
+++ manifests/dell.pp
@@ -2,64 +2,64 @@
 class vendortools::dell {
 
   Package {
-    schedule => daily ,
+    schedule => daily,
   }
 
   yumrepo { 'dell-omsa-indep' :
-    descr          => 'DELL OMSA repository - Hardware independent' ,
-    mirrorlist     => 'http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1&dellsysidpluginver=$dellsysidpluginver' ,
-    gpgkey         => 'http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-dell http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-libsmbios' ,
-    failovermethod => 'priority' ,
-    enabled        => '1' ,
-    gpgcheck       => '1' ,
-    priority       => '10' ,
-    protect        => '0' ,
+    descr          => 'DELL OMSA repository - Hardware independent',
+    mirrorlist     => 'http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1&dellsysidpluginver=$dellsysidpluginver',
+    gpgkey         => 'http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-dell http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-libsmbios',
+    failovermethod => 'priority',
+    enabled        => '1',
+    gpgcheck       => '1',
+    priority       => '10',
+    protect        => '0',
   }
 
 
   yumrepo { 'dell-omsa-specific' :
-    descr          => 'DELL OMSA repository - Hardware specific' ,
-    mirrorlist     => 'http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1&sys_ven_id=$sys_ven_id&sys_dev_id=$sys_dev_id&dellsysidpluginver=$dellsysidpluginver' ,
-    gpgkey         => 'http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-dell http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-libsmbios' ,
-    failovermethod => 'priority' ,
-    enabled        => '1' ,
-    gpgcheck       => '1' ,
-    priority       => '10' ,
+    descr          => 'DELL OMSA repository - Hardware specific',
+    mirrorlist     => 'http://linux.dell.com/repo/hardware/latest/mirrors.cgi?osname=el$releasever&basearch=$basearch&native=1&sys_ven_id=$sys_ven_id&sys_dev_id=$sys_dev_id&dellsysidpluginver=$dellsysidpluginver',
+    gpgkey         => 'http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-dell http://linux.dell.com/repo/hardware/latest/RPM-GPG-KEY-libsmbios',
+    failovermethod => 'priority',
+    enabled        => '1',
+    gpgcheck       => '1',
+    priority       => '10',
   }
 
-  package { [ 'Lib_Utils' , 'MegaCli' ] :
-    ensure  => absent ,
+  package { [ 'Lib_Utils', 'MegaCli' ] :
+    ensure  => absent,
   }
 
-  package { [ 'yum-dellsysid' , 'dell_ft_install' ] :
-    ensure  => latest ,
-    require => Yumrepo['dell-omsa-indep'] ,
+  package { [ 'yum-dellsysid', 'dell_ft_install' ] :
+    ensure  => latest,
+    require => Yumrepo['dell-omsa-indep'],
   }
 
   package { 'srvadmin-base' :
-    ensure  => latest ,
-    require => Package['yum-dellsysid'] ,
+    ensure  => latest,
+    require => Package['yum-dellsysid'],
   }
 
   package { [
-      'srvadmin-storage' ,
-      'srvadmin-omcommon' ,
-      'srvadmin-omacore' ,
-      'srvadmin-storageservices' ,
+      'srvadmin-storage',
+      'srvadmin-omcommon',
+      'srvadmin-omacore',
+      'srvadmin-storageservices',
     ] :
-    ensure  => latest ,
-    require => Package['srvadmin-base'] ,
+    ensure  => latest,
+    require => Package['srvadmin-base'],
   }
 
   package { 'perl-Config-Tiny' :
-    ensure  => latest ,
+    ensure  => latest,
   }
 
   service { 'dataeng' :
-    ensure     => running ,
-    enable     => true ,
-    hasrestart => true ,
-    pattern    => 'dsm_sa_datamgrd' ,
-    require    => Package['srvadmin-storage'] ,
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    pattern    => 'dsm_sa_datamgrd',
+    require    => Package['srvadmin-storage'],
   }
 }
--- manifests/vmware.pp
+++ manifests/vmware.pp
@@ -2,16 +2,16 @@
 class vendortools::vmware {
 
   yumrepo { 'vmware-tools-collection' :
-    descr    => "VMWare ESXi 5.5 Guest Tools for RHEL/CentOS ${operatingsystemmajrelease} ${architecture}" ,
-    baseurl  => "http://packages.vmware.com/tools/esx/5.5/rhel${operatingsystemmajrelease}/${architecture}" ,
-    gpgkey   => 'http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub' ,
-    enabled  => '1' ,
-    gpgcheck => '1' ,
+    descr    => "VMWare ESXi 5.5 Guest Tools for RHEL/CentOS ${operatingsystemmajrelease} ${architecture}",
+    baseurl  => "http://packages.vmware.com/tools/esx/5.5/rhel${operatingsystemmajrelease}/${architecture}",
+    gpgkey   => 'http://packages.vmware.com/tools/keys/VMWARE-PACKAGING-GPG-RSA-KEY.pub',
+    enabled  => '1',
+    gpgcheck => '1',
   }
 
   package { 'vmware-tools-esx-nox' :
-    ensure => installed ,
-    require => Yumrepo['vmware-tools-collection'] ,
+    ensure  => installed,
+    require => Yumrepo['vmware-tools-collection'],
   }
 
 }
