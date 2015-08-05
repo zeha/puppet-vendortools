399802dac0768d2b35ee5098c211b3d73349d213
Christian Hofstaedtler <christian@hofstaedtler.name>
2015-08-05 14:30:01 +0200

    Add Debian and CentOS 7 support

--- manifests/hp.pp
+++ manifests/hp.pp
@@ -1,29 +1,69 @@
 #Copyright (c) 2013-2014 Voalte Inc. All rights reserved.
-class vendortools::hp {
+class vendortools::hp::repo {
+  case $::osfamily {
+    'RedHat': {
+      yumrepo { 'HP-MCP' :
+        descr          => 'HP Software Delivery Repository for MCP',
+        baseurl        => 'http://downloads.linux.hp.com/SDR/downloads/MCP/CentOS/$releasever/$basearch/current',
+        gpgkey         => 'http://downloads.linux.hp.com/SDR/hpPublicKey1024.pub http://downloads.linux.hp.com/SDR/hpPublicKey2048.pub http://downloads.linux.hp.com/SDR/hpPublicKey2048_key1.pub',
+        failovermethod => 'priority',
+        enabled        => '1',
+        gpgcheck       => '1',
+        priority       => '10',
+      }
+    }
+    'Debian': {
+      file { '/etc/apt/HP-MCP.key':
+        source     => 'puppet:///modules/vendortools/hp/HP-MCP-Debian.key',
+      }
 
-  Package {
-    schedule => daily,
-  }
+      apt::key { 'HP-MCP':
+        key_source => '/etc/apt/HP-MCP.key',
+        key        => '882F7199B20F94BD7E3E690EFADD8D64B1275EA3',
+      }
 
-  yumrepo { 'HP-MCP' :
-    descr          => 'HP Software Delivery Repository for MCP',
-    baseurl        => 'http://downloads.linux.hp.com/SDR/downloads/MCP/CentOS/$releasever/$basearch/current',
-    gpgkey         => 'http://downloads.linux.hp.com/SDR/downloads/MCP/GPG-KEY-mcp',
-    failovermethod => 'priority',
-    enabled        => '1',
-    gpgcheck       => '0',
-    priority       => '10',
+      apt::source { 'HP-MCP':
+        location    => 'http://downloads.linux.hp.com/SDR/repo/mcp',
+        repos       => 'non-free',
+        release     => 'trusty/current',
+        key         => '882F7199B20F94BD7E3E690EFADD8D64B1275EA3',
+        include_src => false,
+      }
+    }
   }
+}
+
+class vendortools::hp::params {
+  case $::osfamily {
+    'RedHat': {
+      if versioncmp($::operatingsystemrelease, '7.0') >= 0 {
+        $hpssacli_package_name = 'hpssacli'
+      } else {
+        $hpssacli_package_name = 'hpacucli'
+      }
+    }
+    'Debian': {
+      $hpssacli_package_name = 'hpssacli'
+    }
+  }
+}
+
+class vendortools::hp (
+  $hpssacli_package_name = $::vendortools::hp::params::hpssacli_package_name,
+) inherits ::vendortools::hp::params
+{
+
+  include vendortools::hp::repo
 
   package { 'hp-health' :
     ensure  => latest,
-    require => Yumrepo['HP-MCP'],
+    require => Class['vendortools::hp::repo'],
     notify  => Service['hp-health'],
   }
 
-  package { [ 'hpacucli', 'hponcfg' ] :
+  package { [ $hpssacli_package_name, 'hponcfg' ] :
     ensure  => 'latest',
-    require => Yumrepo['HP-MCP'],
+    require => Class['vendortools::hp::repo'],
   }
 
   service { 'hp-health' :
@@ -39,7 +79,7 @@ class vendortools::hp {
 
   package { 'hp-snmp-agents' :
     ensure   => latest,
-    require  => Yumrepo['HP-MCP'],
+    require  => Class['vendortools::hp::repo'],
     notify   => Service['hp-snmp-agents'],
   }
 
@@ -47,5 +87,4 @@ class vendortools::hp {
     ensure  => running,
     require => Package['hp-snmp-agents'],
   }
-
 }
